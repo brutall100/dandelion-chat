b5caf6469627bc37ebb3e3973692f2ea30fdd26f
brutall100 <37956008+brutall100@users.noreply.github.com>
2023-09-21 23:36:16 +0300

    V-0.0.2
    Server working example.

--- index.php
+++ index.php
@@ -1,14 +1,44 @@
-<section id="chat-container-section">
-      <div id="chat-container-messages">
-        <ul id="chat-messages"></ul>
-        <div id="chat-user-data" chat-data-name="<?php echo isset($name) ? $name : ''; ?>"></div>
-      </div>
-      <div>
-        <form id="chat-form" action="/save-message" method="POST">
-          <input id="chat-input-msg" name="user_message" autocomplete="off" />
-          <!-- <input type="hidden" id="chat-user-id" name="user_id" value="<?php echo isset($user_id) ? $user_id : ''; ?>"> -->
-          <!-- <input type="hidden" id="chat-user-name" name="user_name" value="<?php echo isset($name) ? $name : ''; ?>">  -->
-          <button id="chat-button" type="submit">Send</button>
-        </form>
-      </div>
-    </section>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Dokumentas</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+
+
+<body>
+  <header>
+    <div class='header'>
+      <h3>HEADER</h3>
+    </div>
+  </header>
+
+  <main>
+    <section id="chat-container-section">
+        <div id="chat-container-messages">
+            <ul id="chat-messages"></ul>
+        </div>
+        <div>
+            <form id="chat-form" method="POST" action="http://localhost:9001/save-message" >
+            <input id="chat-input-msg" name="user_message" autocomplete="off" />
+            <button id="chat-button" type="submit">Send</button>
+            </form>
+        </div>
+        </section>
+  </main>
+ 
+  <footer>
+    <div class='footer'>
+      <h3>FOOTER</h3>
+    </div>
+  </footer>
+</body>
+
+  <!-- <script type="text/javascript" src="client.js"></script> -->
+</html>
+
+
+
